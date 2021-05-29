eff17c042486935a8ca91ce2a97e36675683a316
AntoineDebes <antoinedebes@gmail.com>
2021-05-29 19:35:45 +0300

    Fixed some stuffs

--- javascript-basics-02/step 6/step_6.js
+++ javascript-basics-02/step 6/step_6.js
@@ -1,9 +1,11 @@
-var img = document.querySelectorAll("img");
+(function(){
+    var img = document.querySelectorAll("img");
 
-img.forEach(img => img.addEventListener("mouseover", () => {
-    img.src = img.src.replace(".jpg","_2.jpg"); 
-}));
+    img.forEach(img => img.addEventListener("mouseover", () => {
+        img.src = img.src.replace(".jpg","_2.jpg"); 
+    }));
 
-img.forEach(img => img.addEventListener("mouseout", () => {
-    img.src = img.src.replace("_2.jpg",".jpg"); 
-}));
+    img.forEach(img => img.addEventListener("mouseout", () => {
+        img.src = img.src.replace("_2.jpg",".jpg"); 
+    }));
+})();
